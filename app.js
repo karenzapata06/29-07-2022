57ce5b5922d146cdabec397a7bd30781287509c6
karenzapata06 <karenmilenazg@gmail.com>
2022-07-29 16:09:31 -0500

    Javascript

---
+++ app.js
@@ -0,0 +1,39 @@
+/*var nombre = 'Karen zapata';
+
+if (true) {
+    console.log(nombre)
+    let nombre = 'Karen zapata';
+}
+console.log(nombre)*/
+
+//let nombre = "Karen zapata";
+//let edad  = 22;
+
+//let nombreCliente = "Karen";
+//let edadCliente = 22;
+//let profesionCliente = "Estudiante";
+//console.log("console.log("El nombre es " + nombre + " Y la edad es " + edad)
+
+/*let nombre = prompt("Ingrese un nombre; ");
+let edad = prompt("Ingrese edad");
+console.log(" El nombre es: " + nombre + "\n La edad es: " + edad + "años")*/
+
+//console.log(`El estudiante ${nombre} Tiene ${edad} años`);
+
+let cantidadHoras = 5;
+let valorHora = 2000;
+let salario = (valorHora * cantidadHoras)
+
+console.log(salario)
+
+if(cantidadHoras<24) {
+    let aumentoSalario = (valorHora*10/100);
+    let salarioTotal = salario + aumentoSalario;
+    console.log("Su salario con aumento es de: " + salarioTotal)
+}
+
+
+
+
+
+
